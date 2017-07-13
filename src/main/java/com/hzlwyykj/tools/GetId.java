15069dd2d42d5fa73f86cf5e4dc7d660d1ceb686
18662815187 <525455531@qq.com>
2017-07-13 21:54:05 +0800

    上传新增文件2017.7.13

---
+++ src/main/java/com/hzlwyykj/tools/GetId.java
@@ -0,0 +1,24 @@
+package com.hzlwyykj.tools;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GetId {
+	private static long workid = 0;
+	private static long fileNewName = 0;
+
+	public synchronized static String getWorkid() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmSS");
+		String now = sdf.format(new Date());
+		workid++;
+		return now + workid;
+	}
+
+	public synchronized static String getNewFileName() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmSS");
+		String now = sdf.format(new Date());
+		fileNewName++;
+		return now + fileNewName;
+	}
+
+}
